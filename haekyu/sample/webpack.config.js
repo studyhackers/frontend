440ddee52924900cb10fc05b998bcdb3f9954984
haekyu <gorb6593@gmail.com>
2022-10-11 00:46:37 +0900

    2주차 스터디

---
+++ haekyu/sample/webpack.config.js
@@ -0,0 +1,67 @@
+const path = require("path");
+const webpack = require("webpack");
+const chileProcess = require('child_process');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/app.js",
+  },
+  output: {
+    path: path.resolve("./dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css)$/,
+        use: [
+          process.env.NODE_ENV === 'production'
+          ? MiniCssExtractPlugin.loader
+          : "style-loader",
+          "css-loader",
+        ]
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        loader: "url-loader",
+        options: {
+          name: "[name].[ext]?[hash]",
+          limit: 20000 // 20Kb
+        }
+      }
+      
+    ]
+  },
+  plugins:[
+    new webpack.BannerPlugin({
+      banner: `
+        Build Date: ${new Date().toLocaleString()}
+        Commit Version: ${chileProcess.execSync("git rev-parse --short HEAD")}
+        Author: ${chileProcess.execSync("git config user.name")}
+      `
+    }),
+    new webpack.DefinePlugin({
+      TWO: JSON.stringify('1+1'),
+      'api.domain': JSON.stringify('http://dev.api.domain.com')
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      templateParameters: {
+        env: process.env.NODE_ENV === 'development' ? '(개발용)' : ''
+      },
+      minify: process.env.NODE_ENV === 'production' ?{
+        collapseWhitespace: true,
+        removeComments: true,
+      } : false
+    }),
+    new CleanWebpackPlugin(),
+    ...(process.env.NODE_ENV === 'production'
+      ? [new MiniCssExtractPlugin({filename: '[name].css'})]
+      : []
+    )
+  ]
+};
