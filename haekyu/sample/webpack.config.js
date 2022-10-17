0f2bc3029f6c570a50f8829eabc99eb1f5c9eff4
haekyu <gorb6593@gmail.com>
2022-10-18 01:30:53 +0900

    3주차 스터디

--- haekyu/sample/webpack.config.js
+++ haekyu/sample/webpack.config.js
@@ -1,14 +1,14 @@
 const path = require("path");
 const webpack = require("webpack");
-const chileProcess = require('child_process');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const chileProcess = require("child_process");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
   entry: {
-    main: "./src/app.js",
+    main: "./app.js",
   },
   output: {
     path: path.resolve("./dist"),
@@ -19,49 +19,55 @@ module.exports = {
       {
         test: /\.(css)$/,
         use: [
-          process.env.NODE_ENV === 'production'
-          ? MiniCssExtractPlugin.loader
-          : "style-loader",
+          process.env.NODE_ENV === "production"
+            ? MiniCssExtractPlugin.loader
+            : "style-loader",
           "css-loader",
-        ]
+        ],
       },
       {
         test: /\.(png|jpg|svg|gif)$/,
         loader: "url-loader",
         options: {
           name: "[name].[ext]?[hash]",
-          limit: 20000 // 20Kb
-        }
-      }
-      
-    ]
+          limit: 20000, // 20Kb
+        },
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
   },
-  plugins:[
+  plugins: [
     new webpack.BannerPlugin({
       banner: `
         Build Date: ${new Date().toLocaleString()}
         Commit Version: ${chileProcess.execSync("git rev-parse --short HEAD")}
         Author: ${chileProcess.execSync("git config user.name")}
-      `
+      `,
     }),
     new webpack.DefinePlugin({
-      TWO: JSON.stringify('1+1'),
-      'api.domain': JSON.stringify('http://dev.api.domain.com')
+      TWO: JSON.stringify("1+1"),
+      "api.domain": JSON.stringify("http://dev.api.domain.com"),
     }),
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: "./src/index.html",
       templateParameters: {
-        env: process.env.NODE_ENV === 'development' ? '(개발용)' : ''
+        env: process.env.NODE_ENV === "development" ? "(개발용)" : "",
       },
-      minify: process.env.NODE_ENV === 'production' ?{
-        collapseWhitespace: true,
-        removeComments: true,
-      } : false
+      minify:
+        process.env.NODE_ENV === "production"
+          ? {
+              collapseWhitespace: true,
+              removeComments: true,
+            }
+          : false,
     }),
     new CleanWebpackPlugin(),
-    ...(process.env.NODE_ENV === 'production'
-      ? [new MiniCssExtractPlugin({filename: '[name].css'})]
-      : []
-    )
-  ]
+    ...(process.env.NODE_ENV === "production"
+      ? [new MiniCssExtractPlugin({ filename: "[name].css" })]
+      : []),
+  ],
 };
