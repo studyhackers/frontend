d28bcdfd371bbb3023221b303c3b12e86bcc0f43
[개발팀] 박종환 <whdghks94@gmail.com>
2022-10-18 14:45:44 +0900

    3주차 스터디

---
+++ jonghwna/study3/lecture-frontend-dev-env-3-lint-1-eslint/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
