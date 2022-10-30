81f7e0cb6bbdcd373a59b963c21ab9e09b857f0f
vvuru <wown5398@gmail.com>
2022-10-31 01:53:52 +0900

    review_4

--- uwon/sample/src/app.js
+++ uwon/sample/src/app.js
@@ -1,3 +1,9 @@
-import * as math from './math.js';
+import './app.css';
+import nyancat from './nyancat.jpg';
 
-console.log(math.sum(1,2));
+document.addEventListener('DOMContentLoaded',()=>{
+    document.body.innerHTML = `
+        <img src="${nyancat}" />
+    `
+    
+})
